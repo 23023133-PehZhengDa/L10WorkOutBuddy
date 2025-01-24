83a993143f3d3f007890f5f27d7483dd9bb673e1
pehzhengda <23023133@mrp.edu.sg>
2025-01-25 00:01:03 +0800

    update

--- front_end/src/components/WorkoutForm.js
+++ front_end/src/components/WorkoutForm.js
@@ -15,11 +15,13 @@ const WorkoutForm = () => {
 
         const workout = {title, load, reps}
 
-        const response = await fetch("https://l10workoutbuddy-backend-4sqs.onrender.com/api/workouts", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+        const response = await fetch(`${process.env.REACT_APP_API_URL}api/workouts`, {
+            method: 'POST',
             body: JSON.stringify(workout),
-          });
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        })
         const json = await response.json()
 
         if (!response.ok) {
